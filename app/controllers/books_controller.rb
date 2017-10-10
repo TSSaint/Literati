6ede93298a9c4d489eee3d79923c303df59c3e54
Thomas Santos <thomas@socialeffort.co>
2017-10-10 14:48:28 -0400

    fix error in books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  
   def index
   end
 
@@ -9,17 +10,16 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-
     if @book.save
       # successful book add sends user back to index page
       redirect_to root_path
     else
       # otherwise, render a new form
       render 'new'
+    end
   end
 
   private
-    
     # when a user sends information, this params holds that data from some form
     def book_params
       params.require(:book).permit(:title, :description, :author)
