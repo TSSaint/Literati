e21c2112237a5ed23f1ad2fec59d93af22bd2afc
Thomas Santos <thomas@socialeffort.co>
2017-10-10 13:36:52 -0400

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # RESTful Route
   # A method that declares index, show, new, edit, update, and destroy actions instead of declaring separate routes
   resources :books
+  root 'books#index'
 end
