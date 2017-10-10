83b4b7c6df8b4afdaf347cbf9263a1e63ca1cc0e
Thomas Santos <thomas@socialeffort.co>
2017-10-10 13:34:13 -0400

    add RESTful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # RestFUL Route
+  # RESTful Route
   # A method that declares index, show, new, edit, update, and destroy actions instead of declaring separate routes
   resources :books
 end
