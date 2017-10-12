20d48cd0526b9455e52f1a3b7487bab2e365e70a
Thomas Santos <thomas@socialeffort.co>
2017-10-11 21:43:40 -0400

    add user ids to review table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012014145) do
+ActiveRecord::Schema.define(version: 20171012014325) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171012014145) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
