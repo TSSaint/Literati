58c383da7dbbc39e208afdfa783d00816af26efe
Thomas Santos <thomas@socialeffort.co>
2017-10-11 21:58:59 -0400

    generate reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # RESTful Route
   # A method that declares index, show, new, edit, update, and destroy actions instead of declaring separate routes
-  resources :books
+  resources :books do
+    resources :reviews
+    # storing the RESTful resource reviews INSIDE books
+  end
+
   root 'books#index'
 end
